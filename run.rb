29a0a1dc2dd385e3596674a37035bc1f9134c73e
secondbaker <secondbaker@gmail.com>
2020-08-20 10:56:21 -0400

    Saving my place

--- run.rb
+++ run.rb
@@ -1,12 +1,1 @@
-def say_goodbye
-    x = "Goodbye"
-    puts x
-end
-def start_here
-    x = "Hello"
-    puts x
-    say_goodbye
-    puts "Let's check whether x remained the same: "
-    puts x
-end
-start_here
+#pick up from 2.5.1, pdf page 86
