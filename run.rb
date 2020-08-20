2d2188d6db4bdffbc199c96bffa7a7d7f02802b5
secondbaker <secondbaker@gmail.com>
2020-08-20 10:23:33 -0400

    I think I get why Java 'reflection' is called reflection now

--- run.rb
+++ run.rb
@@ -1,10 +1,10 @@
 # Continue from 2.3.1, p.45
 obj = Object.new
 
-puts "The id of obj is #{obj.object_id}."
 
-int = 100
-1000.times do
-    puts int.object_id
-    int = int.object_id
+if obj.respond_to?("talk")
+    obj.talk    
+else
+    puts "Sorry, the object doesn't understand the 'talk' message"
 end
+
