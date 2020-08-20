fed7342b88b893d879aa37fddd23d972441a0fd9
secondbaker <secondbaker@gmail.com>
2020-08-20 10:40:41 -0400

    Using a catch-all but with a minimum number of arguments

--- run.rb
+++ run.rb
@@ -1,7 +1,21 @@
 obj = Object.new
 
-def obj.one_arg(*x)
+def obj.one_arg(x)
+    puts "I require one and only one argument!"
+end
+# obj.one_arg(1, 2, 3) nope
+obj.one_arg(1)
+
+def obj.multi_args(*x)
     puts "I can take zero or more arguments!"
     puts "This time I received: #{x.count}"
 end
-obj.one_arg(1, 2, 3)
+obj.multi_args(1, 2, 3)
+
+def obj.two_or_more(a, b, *c)
+    puts "I require two or more arguments!"
+    puts "This time I received #{a}, #{b}, and #{c}, so that makes #{2 + c.count}"
+end
+obj.two_or_more(1, 2, 3)
+obj.two_or_more(1, 2, 3, 4, 5)
+# obj.two_or_more(1) nope
