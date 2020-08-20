8f094b64181076b6dc37a5b8e262b18039c586c0
secondbaker <secondbaker@gmail.com>
2020-08-20 10:34:21 -0400

    Of course I knew this one already

--- run.rb
+++ run.rb
@@ -1,29 +1,6 @@
-ticket = Object.new
+obj = Object.new
 
-def ticket.date
-    "01/02/03"
+def obj.one_arg(x)
+    puts "I require one and onle one argument!"
 end
-def ticket.venue
-    "Town Hall"
-end
-def ticket.event
-    "Author's reading"
-end
-def ticket.performer
-    "Mark Twain"
-end
-def ticket.seat
-    "Second Balcony, row J, set 12"
-end
-def ticket.price
-    "$#{('%.2f' % 5.50)}"
-end
-
-print "Information desired: "
-request = gets.chomp
-
-if ticket.respond_to?(request)
-    puts ticket.send(request)
-else
-    puts "No such information available"
-end
+obj.one_arg(1, 2, 3)
