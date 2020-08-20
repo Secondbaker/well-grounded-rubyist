775b50149d87185c0abb2a231316331aeca84923
secondbaker <secondbaker@gmail.com>
2020-08-20 10:52:50 -0400

    Variable scope appears at first glance to work the same as C#, Java, etc.

---
+++ method_args.rb
@@ -0,0 +1,39 @@
+obj = Object.new
+
+def obj.one_arg(x)
+    puts "I require one and only one argument!"
+end
+# obj.one_arg(1, 2, 3) nope
+obj.one_arg(1)
+
+def obj.multi_args(*x)
+    puts "I can take zero or more arguments!"
+    puts "This time I received: #{x.count}"
+end
+obj.multi_args(1, 2, 3)
+
+def obj.two_or_more(a, b, *c)
+    puts "I require two or more arguments!"
+    puts "This time I received #{a}, #{b}, and #{c}, so that makes #{2 + c.count}"
+end
+obj.two_or_more(1, 2, 3)
+obj.two_or_more(1, 2, 3, 4, 5)
+# obj.two_or_more(1) nope
+
+def obj.default_args(a, b, c = 1)
+    puts "Values of variables: ", a, b, c
+end
+obj.default_args(1, 2)
+obj.default_args(1, 2, 3) 
+
+def obj.mixed_args(a, b, *c, d)
+    puts "Arguments:"
+    p a, b, c, d
+end
+obj.mixed_args(1, 2, 3, 4, 6, 5, 7, 8, 9)
+
+def obj.mixed_default_args(a, b=1, *c, d, e)
+    puts "Arguments:"
+    p a, b, c, d, e
+end
+obj.mixed_default_args(1, 2, 3)
