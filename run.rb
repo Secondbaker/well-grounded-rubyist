19163df48821d4d9a0244a7b794d5617b89bded6
secondbaker <secondbaker@gmail.com>
2020-08-20 10:42:38 -0400

    Defaults work like I would expect

--- run.rb
+++ run.rb
@@ -19,3 +19,9 @@ end
 obj.two_or_more(1, 2, 3)
 obj.two_or_more(1, 2, 3, 4, 5)
 # obj.two_or_more(1) nope
+
+def obj.default_args(a, b, c = 1)
+    puts "Values of variables: ", a, b, c
+end
+obj.default_args(1, 2)
+obj.default_args(1, 2, 3) 
