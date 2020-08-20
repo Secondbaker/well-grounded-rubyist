e6cc976a0af2d9c6916f18ee90e2ce62be28a479
secondbaker <secondbaker@gmail.com>
2020-08-20 10:35:53 -0400

    Using * creates an array that takes the remainder of the arguments, it would seem

--- run.rb
+++ run.rb
@@ -1,6 +1,7 @@
 obj = Object.new
 
-def obj.one_arg(x)
-    puts "I require one and onle one argument!"
+def obj.one_arg(*x)
+    puts "I can take zero or more arguments!"
+    puts "This time I received: #{x.count}"
 end
 obj.one_arg(1, 2, 3)
